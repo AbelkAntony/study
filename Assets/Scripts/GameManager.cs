304c72c842b5a4d467da2ac376486a07fbdcd205
ABEL K ANTONY <abelkantony98@gmail.com>
2025-02-24 11:50:43 +0530

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int firstNumber = 10;
+    private int secondNumber = 5;
+    private string[] students = {    "ABEL K ANTONY" , 
+                                    "ABEL ABRAHAM TITUS", 
+                                    "ADITYA E AJITH ",
+                                    "ASHMIN",
+                                    "SAI",
+                                    "SREEYAS",
+                                    "SREEJITH",
+                                    "SHABARINATH",
+                                    "ZABRAN",
+                                    "JORDIN",
+                                    "SHYAM",
+                                    "UDIT",
+                                    "DEVANARAYANAN"};
+
+
+    void Start()
+    {
+        //Addition();
+        //Subtraction();
+        //Multiplication();
+        SendMessageUsingForeach();
+        SendMessageUsingForLoop();  
+    }
+
+    void Addition()
+    {
+        int addition = firstNumber + secondNumber;
+        print(firstNumber + "+" + secondNumber + "=" + addition);
+    }
+
+    void Subtraction()
+    {
+        int substraction = firstNumber - secondNumber;
+        print(firstNumber + "-" + secondNumber + "=" + substraction);
+    }
+
+    void Multiplication()
+    {
+        int multiplication = firstNumber * secondNumber;
+        print(firstNumber + "*" + secondNumber + "=" + multiplication);
+    }
+
+    void SendMessageUsingForeach()
+    {
+        foreach(string student in students)
+        {
+            print(student + " come and meet me");
+        }
+    }
+  
+    void SendMessageUsingForLoop()
+    {
+        for(int i =0;i>students.Length;i++)
+        {
+            print(students[i] + "call me urgently");
+        }
+    }
+}
